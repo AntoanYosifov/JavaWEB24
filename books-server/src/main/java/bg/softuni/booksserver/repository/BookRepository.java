29cf8ed540020ba6b2b11385dbaf2400859f38cc
AntoanYosifov <tapaktapxaomi@gmail.com>
2024-06-25 06:57:23 +0200

    Persistence layer

---
+++ books-server/src/main/java/bg/softuni/booksserver/repository/BookRepository.java
@@ -0,0 +1,11 @@
+package bg.softuni.booksserver.repository;
+
+import bg.softuni.booksserver.model.entity.BookEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<BookEntity, Long> {
+
+
+}
